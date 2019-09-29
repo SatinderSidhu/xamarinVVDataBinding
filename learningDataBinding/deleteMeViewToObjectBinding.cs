475a74fff5fe29daeef5818dbb61cf87786c6e56
Satinder Sidhu <satinder.satty@gmail.com>
2019-09-29 19:31:50 -0400

    Add files via upload

---
+++ learningDataBinding/deleteMeViewToObjectBinding.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace learningDataBinding
+{
+    public class deleteMeViewToObjectBinding : ContentPage
+    {
+        public deleteMeViewToObjectBinding()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
---
+++ learningDataBinding/demoViewToObeject.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace learningDataBinding
+{
+    public partial class demoViewToObeject : ContentPage
+    {
+        public demoViewToObeject()
+        {
+            InitializeComponent();
+
+            Employee emp = new Employee() { Address = "Morris Plains", Name = "Satinder SIdhu", Age = "40" };
+
+            iAmLabel.BindingContext = emp;            
+            
+        }
+    }
+
+    /// <summary>
+    /// Simple employee class
+    /// </summary>
+    public class Employee
+    {
+        public string Name;
+        public string Age;
+        public string Address;
+    }
+}
